5780beece28d0809ee5ba91efbeb0d676e01bb34
Serhii Holubtsov <golubtsovsergei581@gmail.com>
2020-10-11 20:19:45 +0300

    Updates

---
+++ precache-manifest.e6ef7ddf841d0c99961ff5606e92b1ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bee60c2944d76599f4f258d19a6c06",
+    "url": "/Todo-React-App/index.html"
+  },
+  {
+    "revision": "79aed0b394c545ee0882",
+    "url": "/Todo-React-App/static/css/main.ff4d3b12.chunk.css"
+  },
+  {
+    "revision": "11e16aa891ce1a0e25e3",
+    "url": "/Todo-React-App/static/js/2.31f48a3b.chunk.js"
+  },
+  {
+    "revision": "ea400bfb7cfb62de59f10ac82e4a6f9d",
+    "url": "/Todo-React-App/static/js/2.31f48a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79aed0b394c545ee0882",
+    "url": "/Todo-React-App/static/js/main.a2207c8f.chunk.js"
+  },
+  {
+    "revision": "1f9a970f2f1fafb5d3ca",
+    "url": "/Todo-React-App/static/js/runtime-main.dda85e4a.js"
+  }
+]);
